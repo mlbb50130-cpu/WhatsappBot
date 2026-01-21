3fa5354ed7b1320c1bc8f2beff389f0c9ffc0122
tetsushark007-hub <tetsushark007@gmail.com>
2026-01-21 23:50:24 +0100

    Fix: Pre-load AdminActionsManager in handler to resolve module loading

--- src/handler.js
+++ src/handler.js
@@ -5,6 +5,7 @@ const User = require('./models/User');
 const CooldownManager = require('./utils/cooldown');
 const XPSystem = require('./utils/xpSystem');
 const PermissionManager = require('./utils/permissions');
+const AdminActionsManager = require('./utils/adminActions');
 
 const commands = new Map();
 
